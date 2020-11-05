f7911093bc86c2eceb4bb301cfbf5af8263a99a6
S.Aravind <63240570+ARAVIND281@users.noreply.github.com>
2020-11-05 06:21:25 +0530

    Add files via upload

--- P-PRO-99/Removefiles.py
+++ P-PRO-99/Removefiles.py
@@ -20,7 +20,7 @@ def main():
 			if seconds >= get_file_or_folder_age(root_folder):
 
 				remove_folder(root_folder)
-				deleted_folders_count += 1 # incrementing count
+				deleted_folders_count += 1 
 
 				break
 
@@ -33,7 +33,7 @@ def main():
 					if seconds >= get_file_or_folder_age(folder_path):
 
 						remove_folder(folder_path)
-						deleted_folders_count += 1 # incrementing count
+						deleted_folders_count += 1 
 
 
 				for file in files:
@@ -43,19 +43,18 @@ def main():
 					if seconds >= get_file_or_folder_age(file_path):
 
 						remove_file(file_path)
-						deleted_files_count += 1 # incrementing count
-
+						deleted_files_count += 1 
 		else:
 
 			if seconds >= get_file_or_folder_age(path):
 
 				remove_file(path)
-				deleted_files_count += 1 # incrementing count
+				deleted_files_count += 1 
 
 	else:
 
 		print(f'"{path}" is not found')
-		deleted_files_count += 1 # incrementing count
+		deleted_files_count += 1 
 
 	print(f"Total folders deleted: {deleted_folders_count}")
 	print(f"Total files deleted: {deleted_files_count}")
